bb36d66c22eccbcabeb0f2bfe9a0f28998365724
redlikeroses <devmaksim2006@gmail.com>
2025-07-27 20:36:50 +0300

    import Post

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,9 +1,13 @@
 __all__ = (
     'Base',
     'User',
-    'Profile'
+    'Profile',
+    'Post',
+
 )
 
 from .base import Base
 from .user import User
 from .profile import Profile
+from .post import Post
+
