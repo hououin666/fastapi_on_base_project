22ede07f06c16f07958f91de8506f5e5171a2eab
redlikeroses <devmaksim2006@gmail.com>
2025-08-06 22:30:23 +0300

    include routers profiles and roles

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .users import router as users_router
 from .posts import router as posts_router
+from .profiles import router as profiles_router
+from .roles import router as roles_router
 
 
 router = APIRouter(
@@ -9,3 +11,5 @@ router = APIRouter(
 
 router.include_router(users_router)
 router.include_router(posts_router)
+router.include_router(profiles_router)
+router.include_router(roles_router)
