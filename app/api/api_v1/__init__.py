f94d751d29c4e8b8285b35f7b68134ca1a353612
redlikeroses <devmaksim2006@gmail.com>
2025-08-27 23:40:54 +0300

    add cart router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -3,6 +3,7 @@ from .users import router as users_router
 from .posts import router as posts_router
 from .profiles import router as profiles_router
 from .roles import router as roles_router
+from .cart import router as cart_router
 
 
 router = APIRouter(
@@ -13,3 +14,4 @@ router.include_router(users_router)
 router.include_router(posts_router)
 router.include_router(profiles_router)
 router.include_router(roles_router)
+router.include_router(cart_router)
