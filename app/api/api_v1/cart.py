056e6ae68ac3d785514fbc66f007614fcc8d0720
redlikeroses <devmaksim2006@gmail.com>
2025-08-31 23:41:00 +0300

    cart

--- app/api/api_v1/cart.py
+++ app/api/api_v1/cart.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
+from starlette.responses import Response
 
 from auth.dependensies import get_current_token_payload_user, get_current_active_auth_user
 from core.models.db_helper import db_helper
@@ -12,15 +13,49 @@ router = APIRouter(
 )
 
 
-@router.get('/user/cart')
+@router.get('/user/cart/')
 def get_cart_by_user(
         request: Request,
         payload = Depends(get_current_token_payload_user),
         user: UserSchema = Depends(get_current_active_auth_user),
 ):
-    return cart_crud.get_cart_by_user(payload=payload, request=request)
+   cart = cart_crud.get_cart_by_user(payload=payload, request=request)
+   return {
+       'cart': cart,
+   }
 
 
 @router.post('/user/cart/add')
-async def add_product_to_cart():
-    pass
+async def add_product_to_cart(
+        request: Request,
+        response: Response,
+        product_id: int,
+        payload = Depends(get_current_token_payload_user),
+        user: UserSchema = Depends(get_current_active_auth_user),
+        session: AsyncSession = Depends(db_helper.session_getter)
+):
+    return await cart_crud.add_product_to_cart(
+        product_id=product_id,
+        request=request, response=response,
+        payload=payload,
+        session=session,
+    )
+
+
+@router.post('/user/cart/delete')
+async def remove_product_from_cart(
+        product_id: int,
+        request: Request,
+        response: Response,
+        session: AsyncSession = Depends(db_helper.session_getter),
+        payload: dict = Depends(get_current_token_payload_user),
+        user: UserSchema = Depends(get_current_active_auth_user)
+):
+    return await cart_crud.remove_product_from_cart(
+        product_id=product_id,
+        request=request,
+        response=response,
+        session=session,
+        payload=payload,
+    )
+
--- app/crud/cart.py
+++ app/crud/cart.py
@@ -1,11 +1,17 @@
+import json
+
 from fastapi import HTTPException
 from fastapi.params import Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
-from starlette.status import HTTP_404_NOT_FOUND
+from starlette.responses import Response
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_410_GONE
 
 from auth.dependensies import get_current_token_payload_user, get_current_active_auth_user
+from core.models import Product
 from core.schemas.user import UserSchema
+from crud.product import get_product_by_id
 
 
 def get_cart_by_user(
@@ -14,12 +20,60 @@ def get_cart_by_user(
 ):
     cart: dict = dict(payload.get('cart'))
     cart_cookie = request.cookies.get('cart')
-    return {
-        'cart': cart,
-        'cart_cookie': cart_cookie,
-            }
+    decode_cart_cookie = json.loads(cart_cookie)
+    return decode_cart_cookie
     # raise HTTPException(
     #     status_code=HTTP_404_NOT_FOUND,
     #     detail='Корзина не найдена!'
     # )
+
+
+async def add_product_to_cart(
+        product_id: int,
+        session: AsyncSession,
+        payload: dict,
+        request: Request,
+        response: Response,
+):
+    product: Product = await get_product_by_id(product_id=product_id, session=session)
+    cart = get_cart_by_user(payload=payload, request=request)
+    if product.count >= 1:
+        if cart.get(f'{product.name}') is None:
+            cart[f'{product.name}'] = 1
+        else:
+            cart[f'{product.name}'] += 1
+        product.count -= 1
+    else:
+        raise HTTPException(
+            status_code=HTTP_410_GONE,
+            detail='Товара нет в наличии'
+        )
+    response.set_cookie(key='cart', value=json.dumps(cart), httponly=True, )
+    await session.commit()
+    return cart
+
+
+async def remove_product_from_cart(
+        product_id: int,
+        session: AsyncSession,
+        payload: dict,
+        request: Request,
+        response: Response,
+):
+    product: Product = await get_product_by_id(product_id=product_id, session=session)
+    cart = get_cart_by_user(payload=payload, request=request)
+    if cart.get(f'{product.name}') is not None:
+        if cart.get(f'{product.name}') == 1:
+            cart.pop(f'{product.name}')
+        else:
+            cart[f'{product.name}'] -= 1
+        product.count += 1
+    else:
+        raise HTTPException(
+            status_code=HTTP_410_GONE,
+            detail='Товар отсутствует в корзине'
+        )
+    response.set_cookie(key='cart', value=json.dumps(cart), httponly=True, )
+    await session.commit()
+    return cart
 
